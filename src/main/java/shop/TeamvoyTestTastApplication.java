ece03ab3c971a84a351b6f7d64e61e78ff6e3421
mordast7 <mordast7@gmail.com>
2022-07-05 19:04:22 +0300

    first commit

---
+++ src/main/java/shop/TeamvoyTestTastApplication.java
@@ -0,0 +1,13 @@
+package shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamvoyTestTastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamvoyTestTastApplication.class, args);
+    }
+
+}
